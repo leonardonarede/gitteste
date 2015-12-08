a41973cd760728c59c1ed8a8d2b0536c7596f9ed
leonardonarede <leonardonarede@gmail.com>
2015-12-08 11:40:36 -0300

    Adicionando Novas saidas de texto

--- src/gitteste/Gitteste.java
+++ src/gitteste/Gitteste.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -22,26 +21,3 @@ public class Gitteste {
     }
     
 }
-=======
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package gitteste;
-
-/**
- *
- * @author leoma
- */
-public class Gitteste {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        System.out.println("Francisco");
-    }
-    
-}
->>>>>>> origin/master
