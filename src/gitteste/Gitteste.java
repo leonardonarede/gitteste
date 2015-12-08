f3e1277965919e2a04bdf974e2b2c72a7c845370
Aurea <Aurea@192.168.101.150>
2015-12-08 11:21:20 -0300

    Alterado o nome do usuário

--- src/gitteste/Gitteste.java
+++ src/gitteste/Gitteste.java
@@ -15,7 +15,7 @@ public class Gitteste {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Leonardo");
+        System.out.println("Francisco");
     }
     
 }
