dac79bdf3a23e6df7fd4982574499f8e0e956b9a
leonardonarede <leonardonarede@gmail.com>
2015-12-08 11:11:40 -0300

    Publicando projeto

---
+++ src/gitteste/Gitteste.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gitteste;
+
+/**
+ *
+ * @author leoma
+ */
+public class Gitteste {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Leonardo");
+    }
+    
+}
