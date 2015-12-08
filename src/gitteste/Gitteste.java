73a4b2d8fc6155306e7321ae862e1724c7643421
chagaschico <chagas_chico@hotmail.com>
2015-12-08 11:44:24 -0300

    Ve aii
    TESTE

--- src/gitteste/Gitteste.java
+++ src/gitteste/Gitteste.java
@@ -24,37 +24,7 @@ public class Gitteste {
         System.out.println("Leonardo");
         System.out.println("Francisco colaborador do projeto");
         System.out.println("hahaha tá dando certo");
-    }
-    
-<<<<<<< HEAD
-}
-=======
->>>>>>> origin/master
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package gitteste;
-
-/**
- *
- * @author leoma
- */
-public class Gitteste {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        System.out.println("Francisco");
+        System.out.println("VE AII");
     }
     
 }
-<<<<<<< HEAD
-=======
->>>>>>> origin/master
->>>>>>> origin/master
-=======
-}
->>>>>>> origin/master
